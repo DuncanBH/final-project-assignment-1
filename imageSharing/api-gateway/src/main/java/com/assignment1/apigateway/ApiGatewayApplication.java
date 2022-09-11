12e46c3f0855748e5641f1507c5f613b04c256a3
DuncanBH <2031593@champlaincollege.qc.ca>
2022-09-11 19:45:39 -0400

    Bypass for localhost CORS issue

--- imageSharing/api-gateway/src/main/java/com/assignment1/apigateway/ApiGatewayApplication.java
+++ imageSharing/api-gateway/src/main/java/com/assignment1/apigateway/ApiGatewayApplication.java
@@ -2,6 +2,9 @@ package com.assignment1.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ApiGatewayApplication {
@@ -10,4 +13,14 @@ public class ApiGatewayApplication {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                //Allow CORS to local react app - revisit later for other apps?
+                registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
