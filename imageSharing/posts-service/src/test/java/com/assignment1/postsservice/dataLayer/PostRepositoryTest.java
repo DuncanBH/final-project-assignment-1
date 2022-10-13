87755f44b249e1066a18cb621471832f7fab6490
Clément Cadieux <cadieuxclement@gmail.com>
2022-10-13 10:31:44 -0400

    First Working PostRepositoryTest

---
+++ imageSharing/posts-service/src/test/java/com/assignment1/postsservice/dataLayer/PostRepositoryTest.java
@@ -0,0 +1,54 @@
+package com.assignment1.postsservice.dataLayer;
+
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.test.context.TestPropertySource;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataMongoTest
+@TestPropertySource(properties = "spring.mongodb.embedded.version=3.5.5")
+class PostRepositoryTest {
+    @Autowired
+    public PostRepository postRepository;
+
+
+    @Test
+    void findPostByPostId() {
+        Post post = new Post();
+        post.setPostId(1);
+        post.setCaption("Amaziiin Time");
+        post.setChannel(10);
+        post.setImageId(100);
+
+        Publisher<Post> setup =
+                postRepository.deleteAll().thenMany(postRepository.save(post));
+
+        Publisher<Post> find = postRepository.findPostByPostId(1);
+
+        Publisher<Post> composite = Mono.from(setup).thenMany(find);
+
+        StepVerifier
+                .create(composite)
+                .consumeNextWith(foundPost ->{
+                    assertEquals(post.getPostId(),foundPost.getPostId());
+                    assertEquals(post.getImageId(),foundPost.getImageId());
+                    assertEquals(post.getCaption(),foundPost.getCaption());
+                    assertEquals(post.getChannel(),foundPost.getChannel());
+                }).verifyComplete();
+
+
+    }
+
+    @Test
+    void deletePostByPostId() {
+    }
+
+    @Test
+    void findPostsByChannel() {
+    }
+}
